0a469475b002c88f71aae401d955940ac53fac67
wahidul60 <132948662+wahidul60@users.noreply.github.com>
2025-11-19 00:00:11 +0600

    Enhance solution with additional features
    Added new features including unique value extraction and product price calculation.

--- solution.ts
+++ solution.ts
@@ -9,7 +9,6 @@ const formatValue = (value: string | number | boolean) => {
     if (typeof value === "boolean") {
         return (!value) + ';';
     }
-
 }
 
 
@@ -35,34 +34,108 @@ const person2 = new Person('Alice', 25);
 
 
 type books = {
-    title : string;
-    rating : number;
+    title: string;
+    rating: number;
 }
-let filterByRating =(items:books[]) : books[] => {
-    return (items.filter(item => item.rating >= 4)) 
+let filterByRating = (items: books[]): books[] => {
+    return (items.filter(item => item.rating >= 4))
 };
 const books = [
-  { title: 'Book A', rating: 4.5 },
-  { title: 'Book B', rating: 3.2 },
-  { title: 'Book C', rating: 5.0 },
-]; 
+    { title: 'Book A', rating: 4.5 },
+    { title: 'Book B', rating: 3.2 },
+    { title: 'Book C', rating: 5.0 },
+];
 
 
 
 type user = {
     id: number;
-    name : string;
-    email : string;
-    isActive : boolean;
+    name: string;
+    email: string;
+    isActive: boolean;
 }
-let filterActiveUsers =(user:user[]) :user[]=>  {
-   return user.filter(item => item.isActive)
+let filterActiveUsers = (user: user[]): user[] => {
+    return user.filter(item => item.isActive)
 }
 const users = [
-  { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-  { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-  { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
 ];
 
 
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
+}
+let printBookDetails = (book: Book) => {
+    console.log(`Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${book.isAvailable ? "Yes" : "No"} `)
+}
+const myBook: Book = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    publishedYear: 1925,
+    isAvailable: true,
+};
+
+
+type arrType = (number | string)[];
+type valueType = (number | string);
+
+let getUniqueValues = (array1: arrType, array2: arrType) => {
+    const resultArray: arrType = [];
+    function ifExist(data: valueType): boolean {
+        for (let j = 0; j < resultArray.length; j++) {
+            if (resultArray[j] === data) {
+                return true
+            }
+        } return false;
+    }
+    for (let j = 0; j < array1.length; j++) {
+        if (!ifExist(array1[j])) {
+            resultArray[resultArray.length] = array1[j]
+        }
+    }
+    for (let j = 0; j < array2.length; j++) {
+        if (!ifExist(array2[j])) {
+            resultArray[resultArray.length] = array2[j];
+        }
+    }
+    return resultArray;
+}
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+
+
+interface Product {
+    name: string;
+    price: number;
+    quantity: number;
+    discount?: number;
+}
+
+const calculateTotalPrice = (products: Product[]): number => {
+    if (products.length === 0) {
+        return 0;
+    }
+    const prices = products.map((item) => {
+        let total = item.price * item.quantity;
+        if (item.discount && item.discount >= 0 && item.discount <= 100) {
+            let cut = (total * item.discount) / 100;
+            total = total - cut;
+        }
+        return total;
+    });
+
+    const final = prices.reduce((sum, cur) => sum + cur, 0);
+    return final;
+};
+
+const products = [
+    { name: "Pen", price: 10, quantity: 2 },
+    { name: "Notebook", price: 25, quantity: 3, discount: 10 },
+    { name: "Bag", price: 50, quantity: 1, discount: 20 }
+];
 
